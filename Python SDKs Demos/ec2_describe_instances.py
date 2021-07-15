6d8b0c3069de8691975effc583e75187796f3818
hasherwi <70494788+hasherwi@users.noreply.github.com>
2021-07-15 16:54:15 -0600

    Update ec2_describe_instances.py
    Added demo notes.

--- Python SDKs Demos/ec2_describe_instances.py
+++ Python SDKs Demos/ec2_describe_instances.py
@@ -2,8 +2,12 @@
 
 #First, demo the EC2 dashboard in the AWS Management Console.
 #Second, demo the equivalent AWS CLI call:
-#   aws ec2 describe-instances
-#Third, demo this code in Cloud9, run with Python 3
+    #aws ec2 describe-instances
+    #Optional extra call:
+        #aws ec2 describe-instances --query Reservations[*].Instances[*].InstanceId
+#Third, demo this code in Cloud9
+    #Need to run 'pip install boto3' from terminal first
+    #Then, run with Python 3
 
 import boto3
 ec2 = boto3.client('ec2')
